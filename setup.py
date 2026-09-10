7607f9c30bda730735ae73f31dcfb4ad02612635
Matthew Gleeson <gleesonm@berkeley.edu>
2026-09-10 08:25:07 -0700

    Matplotlib requirement for plotting functions add

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
             'pandas',
             'numpy',
-            'matplotlib',
+            'matplotlib<3.9',
             'scikit-learn',
             'scipy',
             'tinynumpy',
